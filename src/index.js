729c58a8d484fe37890f2a9aab20eeaff4a8e559
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-03-09 10:07:59 +0100

    remove hardcoded leaderBoard list

--- src/index.js
+++ src/index.js
@@ -2,38 +2,6 @@
 import addScore from './modules/addScore.js';
 import render from './modules/renderScore.js';
 
-const playerScores = [
-  {
-    user: 'john',
-    score: '100',
-  },
-  {
-    user: 'jane',
-    score: '20',
-  },
-  {
-    user: 'jill',
-    score: '50',
-  },
-  {
-    user: 'jones',
-    score: '78',
-  },
-  {
-    user: 'joan',
-    score: '125',
-  },
-  {
-    user: 'joy',
-    score: '77',
-  },
-  {
-    user: 'joyce',
-    score: '42',
-  },
-];
-playerScores.forEach((e) => render(e));
-
 const leaderForm = document.getElementById('leaderForm');
 
 leaderForm.addEventListener('submit', (e) => {
