087c063869e6c9d253da6dfeba0b997b1d6e48b2
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-03-09 10:12:08 +0100

    implement refresh button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-// import './style.css';
+import './style.css';
 import addScore from './modules/addScore.js';
-import render from './modules/renderScore.js';
+import refresh from './modules/getScores.js';
 
 const leaderForm = document.getElementById('leaderForm');
+const gameId = 'Zp9s2JGhfxRWLDm5nQyb';
+const refreshBtn = document.getElementById('refresh');
 
 leaderForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  addScore(leaderForm.nameInput.value, leaderForm.scoreInput.value);
+  addScore(gameId, leaderForm.nameInput.value, leaderForm.scoreInput.value)
+    .then(() => {
+      leaderForm.nameInput.value = '';
+      leaderForm.scoreInput.value = '';
+    });
+});
+
+refreshBtn.addEventListener('click', () => {
+  refresh(gameId);
 });
